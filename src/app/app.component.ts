64eadfdd12f88fb8cca3a6c20151a18393949fee
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-11-23 23:10:09 -0500

    style fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../core/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ashley';
+
+  constructor() {
+    // Set the theme to light
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
 }
---
+++ src/core/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  activeNav: string = "#";
+
+  setActiveNav(nav: string) {
+    this.activeNav = nav;
+  }
+}
