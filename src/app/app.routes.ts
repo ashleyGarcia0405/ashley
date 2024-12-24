8feb706e8270b45ffa03806cd99b3ce0e6280eb6
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-12-24 02:31:52 -0500

    fix: navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from '../components/about/about.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { BlogComponent } from '../components/blog/blog.component';
 
 export const routes: Routes = [
   { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'blog', component: BlogComponent }
+//   { path: 'contact', component: ContactComponent }
+//   { path: '', redirectTo: '/about', pathMatch: 'full' }
 ];
--- src/core/navbar/navbar.component.ts
+++ src/core/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
