fdda2256639896b538c0a469f28f9a85181de234
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-12-28 01:05:36 -0500

    testing import

--- src/core/navbar/navbar.component.ts
+++ src/core/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule, RouterModule],
+  imports: [MatIconModule, RouterModule, NgxPageScrollCoreModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
