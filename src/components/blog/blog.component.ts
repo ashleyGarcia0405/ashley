f633c2245e9febf821d602f0ed67e7f17c1cd20f
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-12-29 21:33:02 -0500

    testing: data for gallery

--- src/components/blog/blog.component.ts
+++ src/components/blog/blog.component.ts
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  blogPosts = [
+    {
+      id: '1',
+      title: 'Designing My Portfolio',
+      summary: 'Planning and designing my personal portfolio.',
+      content: 
+        'My portfolio will showcase my projects, achievements, and skills in a clean, minimalist design.',
+      expanded: false,
+    },
+    {
+      id: '2',
+      title: 'Designing My Portfolio',
+      summary: 'Planning and designing my personal portfolio.',
+      content: 
+        'My portfolio will showcase my projects, achievements, and skills in a clean, minimalist design.',
+      expanded: false,
+    },
+    {
+      id: '3',
+      title: 'Designing My Portfolio',
+      summary: 'Planning and designing my personal portfolio.',
+      content: 
+        'My portfolio will showcase my projects, achievements, and skills in a clean, minimalist design.',
+      expanded: false,
+    },
+  ];
 
+  toggleExpand(post:any) {
+    post.expanded = !post.expanded;
+  }
 }
