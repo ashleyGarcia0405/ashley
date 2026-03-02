18cdfd0010c399dd663d3257e7ac5e972d0644a3
Ashley Garcia <ashley.garcia.official@gmail.com>
2026-03-02 04:05:44 -0500

    feat: blog posts

---
+++ src/components/blog/blog-post-layout/blog-post-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-blog-post-layout',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './blog-post-layout.component.html',
+  styleUrls: ['./blog-post-layout.component.css']
+})
+export class BlogPostLayoutComponent {
+  @Input() title!: string;
+  @Input() date!: string;
+  @Input() readingTime!: number;
+}
